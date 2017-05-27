d1caab5d3587a451970ecb0bad5335c273f0a285
Remigijus Grigaliunas <remigijusgrigaliunas@outlook.com>
2017-05-28 00:15:15 +0100

    Created base class for the library and added the structure of the methods.

---
+++ APIKings.Holiday/APIKings.Holiday/HolidayAPI.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APIKings.Holiday
+{
+    public class HolidayAPI
+    {
+        private readonly string APIKey;
+        private readonly string BaseURL;
+        private readonly string BaseURLWithKey;
+
+        public HolidayAPI(string apiKey)
+        {
+            APIKey = apiKey;
+            BaseURL = "http://holiday.apikings.com/";
+            BaseURLWithKey = BaseURL + "/" + APIKey;
+        }
+
+        public DateTime Next(HolidayAPICountries country)
+        {
+
+
+
+            return new DateTime();
+        }
+
+        public DateTime Previous(HolidayAPICountries country)
+        {
+            return new DateTime();
+        }
+
+        public DateTime Past(HolidayAPICountries country)
+        {
+            return new DateTime();
+        }
+
+        public DateTime Future(HolidayAPICountries country)
+        {
+            return new DateTime();
+        }
+
+        public DateTime IsHoliday(HolidayAPICountries country)
+        {
+            return new DateTime();
+        }
+
+        public DateTime Get(HolidayAPICountries country)
+        {
+            return new DateTime();
+        }
+    }
+}
